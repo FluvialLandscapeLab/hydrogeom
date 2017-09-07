cb8c2a71b04c227884d19b994cbd6af9f79f55ab
Geoffrey Poole <gcpoole@users.noreply.github.com>
2017-09-07 16:34:01 -0600

    Commented out depricated function HyporheicBinStats3

--- R/binStats.R
+++ R/binStats.R
@@ -93,43 +93,43 @@ meanRTError = function(meanRT, b, startRT, endRT, maxRT, integrateQ, infiniteAqu
 # hyporheicQ = gross exchange rate across stream bed for flow paths with total residence time between minRT and maxRT
 # porosity = porosity (fraction void space) of aquifer.
 # infiniteAqulfer = Assume aquifer is infinite if T, assume linear decrease to aquifer volume = 0 at maxRT if F.
-getHyporheicBinStats3 = function(nbins, factor, minRT, maxRT, aquiferVol, hyporheicQ, porosity, infiniteAquifer = T) {
-
-  if(infiniteAquifer) {
-    integrateQ = function(b,s,e,m) (e^(b+1) - s^(b+1))/(b+1)
-    integrateV = function(b,s,e,m) (e^(b+2) - s^(b+2))/(b+2)
-  } else {
-    integrateQ = function(b,s,e,m) (e^(b+1)/(b+1)-e^(b+2)/((b+2)*m)) - (s^(b+1)/(b+1)-s^(b+2)/((b+2)*m))
-    integrateV = function(b,s,e,m) (e^(b+2)/(b+2)-e^(b+3)/((b+3)*m)) - (s^(b+2)/(b+2)-s^(b+3)/((b+3)*m))
-  }
-
-  # find b where user-specified hyporheicQ/aquiferWaterStorage = integrationOfQ(minRT-->maxRT)/integrationOfS(minRT-->maxRT)
-  b = optimize(f = bError, hyporheicQ = hyporheicQ, aquiferVol = aquiferVol, porosity = porosity, minRT = minRT, maxRT = maxRT, integrateQ = integrateQ, integrateV = integrateV, lower = -1.999999999, upper = -1.00000001)$minimum
-
-  rtBinBreaks = getBinBreaks(nbins, factor, maxRT - minRT) + minRT
-  L = length(rtBinBreaks)
-  binRTStarts = rtBinBreaks[1:(L-1)]
-  binRTEnds = rtBinBreaks[2:L]
-  binRTs = binRTEnds - binRTStarts
-
-  binReturnFlows = mapply(integrateQ, b, binRTStarts, binRTEnds, maxRT)
-  binVols = mapply(integrateV, b, binRTStarts, binRTEnds, maxRT) / porosity
-
-  binMeanFlowPathResTime = mapply(optimize, startRT = binRTStarts, endRT = binRTEnds, lower = binRTStarts, upper = binRTEnds, MoreArgs = list(f = meanRTError, b = b, maxRT = maxRT, integrateQ = integrateQ, infiniteAquifer = infiniteAquifer))
-  binMeanBinResTime = unlist(binMeanFlowPathResTime["minimum",]) - binRTStarts
-  a = hyporheicQ / sum(binReturnFlows)
-  binReturnFlows = a * binReturnFlows
-  binVols = a * binVols
-
-  rtdStats = list(a=a, b=b)
-  results = data.frame(
-    from = binRTStarts,
-    to = binRTEnds,
-    volume = binVols,
-    meanFlowPathResTime = unlist(binMeanFlowPathResTime["minimum",]),
-    meanBinResTime = binMeanBinResTime,
-    returnFlow = binReturnFlows
-  )
-  attributes(results) = c(attributes(results), rtdStats)
-  return(results)
-}
+# getHyporheicBinStats3 = function(nbins, factor, minRT, maxRT, aquiferVol, hyporheicQ, porosity, infiniteAquifer = T) {
+#
+#   if(infiniteAquifer) {
+#     integrateQ = function(b,s,e,m) (e^(b+1) - s^(b+1))/(b+1)
+#     integrateV = function(b,s,e,m) (e^(b+2) - s^(b+2))/(b+2)
+#   } else {
+#     integrateQ = function(b,s,e,m) (e^(b+1)/(b+1)-e^(b+2)/((b+2)*m)) - (s^(b+1)/(b+1)-s^(b+2)/((b+2)*m))
+#     integrateV = function(b,s,e,m) (e^(b+2)/(b+2)-e^(b+3)/((b+3)*m)) - (s^(b+2)/(b+2)-s^(b+3)/((b+3)*m))
+#   }
+#
+#   # find b where user-specified hyporheicQ/aquiferWaterStorage = integrationOfQ(minRT-->maxRT)/integrationOfS(minRT-->maxRT)
+#   b = optimize(f = bError, hyporheicQ = hyporheicQ, aquiferVol = aquiferVol, porosity = porosity, minRT = minRT, maxRT = maxRT, integrateQ = integrateQ, integrateV = integrateV, lower = -1.999999999, upper = -1.00000001)$minimum
+#
+#   rtBinBreaks = getBinBreaks(nbins, factor, maxRT - minRT) + minRT
+#   L = length(rtBinBreaks)
+#   binRTStarts = rtBinBreaks[1:(L-1)]
+#   binRTEnds = rtBinBreaks[2:L]
+#   binRTs = binRTEnds - binRTStarts
+#
+#   binReturnFlows = mapply(integrateQ, b, binRTStarts, binRTEnds, maxRT)
+#   binVols = mapply(integrateV, b, binRTStarts, binRTEnds, maxRT) / porosity
+#
+#   binMeanFlowPathResTime = mapply(optimize, startRT = binRTStarts, endRT = binRTEnds, lower = binRTStarts, upper = binRTEnds, MoreArgs = list(f = meanRTError, b = b, maxRT = maxRT, integrateQ = integrateQ, infiniteAquifer = infiniteAquifer))
+#   binMeanBinResTime = unlist(binMeanFlowPathResTime["minimum",]) - binRTStarts
+#   a = hyporheicQ / sum(binReturnFlows)
+#   binReturnFlows = a * binReturnFlows
+#   binVols = a * binVols
+#
+#   rtdStats = list(a=a, b=b)
+#   results = data.frame(
+#     from = binRTStarts,
+#     to = binRTEnds,
+#     volume = binVols,
+#     meanFlowPathResTime = unlist(binMeanFlowPathResTime["minimum",]),
+#     meanBinResTime = binMeanBinResTime,
+#     returnFlow = binReturnFlows
+#   )
+#   attributes(results) = c(attributes(results), rtdStats)
+#   return(results)
+# }
